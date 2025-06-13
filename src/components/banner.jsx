78cc0aca9b7e0cd67431994f02ef01d98b315b31
Vanessa Onyinye Ezeh <vanessaezeh@meta.com>
2025-06-13 11:40:09 -0700

    Added styling to the trailer container and also the buttons(all, favorites, watched

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 
-function Banner ({searchTerm, handleSearchChange, handleSearchClick, handleClearClick}) {
-    return (
-        <section className="banner">
-            <div className="search-bar">
-                <input 
-                type="text" 
-                value={searchTerm}
-                onChange={handleSearchChange}
-                placeholder="Search for a movie"
-                />
-                <button onClick={handleSearchClick}>Search</button>
-                <button onClick={handleClearClick}>Clear</button>
-            </div>
-        </section>
-    );
+function Banner({
+  searchTerm,
+  handleSearchChange,
+  handleSearchClick,
+  handleClearClick,
+}) {
+  return (
+    <section className="banner">
+      <div className="search-bar">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          placeholder="Search for a movie"
+        />
+        <button onClick={handleSearchClick}>Search</button>
+        <button onClick={handleClearClick}>Clear</button>
+      </div>
+    </section>
+  );
 }
 
-export default Banner
+export default Banner;
